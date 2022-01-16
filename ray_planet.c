78be498461c4dc76f6abea666c22fefea11c977c
Heinrich Hartmann <derhein@gmail.com>
2022-01-16 12:19:01 +0000

    VTail

--- ray_planet.c
+++ ray_planet.c
@@ -55,10 +55,59 @@ Vector2 sim2scr(Vector2 a) {
 
 int FPS = 100;
 
+typedef struct {
+  int n;
+  int fill;      // contraint: fill < n
+  Vector2 *data; // contraint: points to allocated n-elements
+} VTail;
+
+void VTail_print(VTail *tp) {
+  printf("VTail<n=%d,fill=%d,data=%p>", tp->n, tp->fill, tp->data);
+}
+
+VTail VTail_alloc(int n) {
+  VTail out;
+  out.n = n;
+  out.fill = 0;
+  out.data = calloc(n, sizeof(Vector2));
+  return out;
+}
+
+void VTail_clear(VTail *t) { t->fill = 0; }
+
+void VTail_push(VTail *t, Vector2 a) {
+  // advance elements in t.data
+  for (int tar = t->fill; tar > 0; tar--) {
+    int src = tar - 1;
+    if (src >= 0 && tar < t->n) {
+      t->data[tar] = t->data[src];
+    }
+  }
+  if (t->fill < t->n) {
+    t->fill += 1;
+  }
+  t->data[0] = a;
+}
+
+typedef struct {
+  double y[4];
+  VTail tail;
+  Color color;
+} Planet;
+
+Planet *Planet_alloc(Color color, Vector2 pos, Vector2 vel) {
+  Planet *out = calloc(1, sizeof(Planet));
+  out->color = color;
+  out->y[0] = pos.x;
+  out->y[1] = vel.x;
+  out->y[2] = pos.y;
+  out->y[3] = vel.y;
+  return out;
+}
+
 int main(void) {
   InitWindow(screenWidth, screenHeight, "Raylib Planets");
   SetTargetFPS(FPS);
-  ToggleFullscreen();
 
   int params = 10;
   gsl_odeiv2_system sys = {func, NULL, 4, &params};
@@ -74,9 +123,14 @@ int main(void) {
   Vector2 ballPosition = {0};
   Vector2 mousePos0;
   float t_step = 2.0f / (float)FPS;
+  int tail_len = 100;
+  int tail_skip = 2;
+  VTail tail = VTail_alloc(tail_len);
+  int frame_cnt = 0;
 
   while (!WindowShouldClose()) // Detect window close button or ESC key
   {
+    frame_cnt += 1;
     BeginDrawing();
     ClearBackground(RAYWHITE);
 
@@ -95,6 +149,7 @@ int main(void) {
       y[3] = d.y;
       printf("set to (%.3f + %.3f, %.3f + %.3f);\n", y[0], y[1], y[2], y[3]);
       select = 0;
+      VTail_clear(&tail);
     }
     if (select) {
       DrawCircleV(mousePos0, 2, MAROON);
@@ -104,9 +159,21 @@ int main(void) {
       gsl_odeiv2_driver_apply(d, &t, t_next, y);
       // printf("sim @ (%.3f + %.3f, %.3f + %.3f);\n", y[0], y[1], y[2], y[3]);
       Vector2 a = {.x = y[0], .y = y[2]};
-      Vector2 b = {.x = y[0] + t_step * y[1], .y = y[2] + t_step * y[3]};
       DrawCircleV(sim2scr(a), 4, MAROON);
-      // DrawLineV(sim2scr(a), sim2scr(b), MAROON);
+
+      // Draw velocity
+      float h = 5; // v points h frames in advance
+      Vector2 b = {.x = y[0] + h * t_step * y[1],
+                   .y = y[2] + h * t_step * y[3]};
+      DrawLineV(sim2scr(a), sim2scr(b), BLUE);
+
+      // Draw tail
+      if (frame_cnt % tail_skip == 1) {
+        VTail_push(&tail, sim2scr(a));
+      }
+      for (int i = 0; i < tail.fill; i++) {
+        DrawCircleV(tail.data[i], 1, MAROON);
+      }
     }
     Vector2 orig = {0};
     DrawCircleV(sim2scr(orig), 5, BLACK);
